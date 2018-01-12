e9d3b22f07d0331c9654fd017a272f0b4788deba
James Kirsch <jameskirschaf@gmail.com>
2018-01-12 11:43:13 -0800

    Added Properties and Methods to DrawPile Constructor

--- deal-52/game.js
+++ deal-52/game.js
@@ -1,1 +1,52 @@
+function DrawPile() {
 
+    /***************************************
+     * array of cards
+     * create deck
+     * deal card to player
+     * receive cards from discard
+     * shuffle deck
+     */
+    this.deckOfCards = [];
+
+    function Card(value , suit){
+        this.value = value;
+        this.suit  = suit;
+    }
+
+    this.createDeck = function () {
+        for (var createDeckIndex = 1; createDeckIndex <= 52; createDeckIndex++) {
+            deckOfCards.push(createDeckIndex);
+        }
+        console.log(deckOfCards);
+    };
+
+    this.dealCard = function (sender, numberOfCards) {
+        console.log("Sender: " + sender + "Number of Cards: " + numberOfCards);
+        for (var i = 0; i < numberOfCards; i++) {
+            if(deckOfCards.length>0){
+                this.rebuildDeck();
+            }
+            sender.receiveCards(deckOfCards[i]);
+        }
+    };
+
+    this.rebuildDeck = function (array){
+        deckOfCards = this.shuffleDeck(array);
+        console.log(deckOfCards);
+    };
+
+    this.shuffleDeck = function(array){
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
+
+
+}
+
+var drawPile = new DrawPile();
