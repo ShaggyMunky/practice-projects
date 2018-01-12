1d64c53852bbff61ab9bbf4dc7ab0a9a99340ba8
James Kirsch <jameskirschaf@gmail.com>
2018-01-12 12:34:41 -0800

    saving

--- deal-52/game.js
+++ deal-52/game.js
@@ -9,34 +9,45 @@ function DrawPile() {
      */
     this.deckOfCards = [];
 
-    function Card(value , suit){
+    function Card(value, suit, numVal) {
         this.value = value;
-        this.suit  = suit;
+        this.suit = suit;
+        this.numVal = numVal;
     }
 
     this.createDeck = function () {
-        for (var createDeckIndex = 1; createDeckIndex <= 52; createDeckIndex++) {
-            deckOfCards.push(createDeckIndex);
+        for (var createDeckIndexO = 1; createDeckIndexO <= 4; createDeckIndexO++) {
+            for( var createDeckIndexI = 1 ; createDeckIndexI <=12 ; createDeckIndexI++){
+                if(createDeckIndexI = 1){
+                    var card = new Card('Ace', '');
+                }
+
+
+                var card = new Card();
+                deckOfCards.push(createDeckIndex);
+            }
         }
         console.log(deckOfCards);
     };
 
     this.dealCard = function (sender, numberOfCards) {
+        var cardArray = [];
         console.log("Sender: " + sender + "Number of Cards: " + numberOfCards);
         for (var i = 0; i < numberOfCards; i++) {
-            if(deckOfCards.length>0){
+            if (deckOfCards.length > 0) {
                 this.rebuildDeck();
             }
-            sender.receiveCards(deckOfCards[i]);
+            cardArray.push(deckOfCards[i]);
         }
+        sender.receiveCards(cardArray);
     };
 
-    this.rebuildDeck = function (array){
+    this.rebuildDeck = function (array) {
         deckOfCards = this.shuffleDeck(array);
         console.log(deckOfCards);
     };
 
-    this.shuffleDeck = function(array){
+    this.shuffleDeck = function (array) {
         for (var i = array.length - 1; i > 0; i--) {
             var j = Math.floor(Math.random() * (i + 1));
             var temp = array[i];
@@ -50,65 +61,81 @@ function DrawPile() {
 }
 
 var drawPile = new DrawPile();
-=======
-/**
-*Player
-*/
-function Player(name){
-    this.hand = [];
-    this.playerTurn = function(){
-      console.log(this.hand);
-      var cardsToRemove = prompt("Choose your cards to remove");
-    };
-    this.recieveCards = function (cards){
-        this.hand = this.hand.concat(cards);
-    };
-    this.sendCards = function(target){
 
+
+/*****************************************************
+ *Player
+ */
+function Player(name) {
+
+    this.hand = [];
+
+    this.playerTurn = function () {
+        console.log(this.hand);
+        var cardsToRemove = prompt("Choose your cards to remove");
+        this.draw(cardsToRemove);
+    };
+
+    this.receiveCards = function (card) {
+        this.hand = this.hand.concat(card);
+    };
+
+    this.discardCards = function (target) {
+        discardPile.
+    };
+
+    this.draw = function(numberOfCards){
+        drawPile.dealCard(this, numberOfCards);
     };
 }
+
 /***************************************************************************************************
-* Game
-* @params {undefined}
-* @returns: {undefined} none
-* creates game instance, prompts user for number of players, call FillDeck ,
-*/
+ * Game
+ * @params {undefined}
+ * @returns: {undefined} none
+ * creates game instance, prompts user for number of players, call FillDeck ,
+ */
 
-function Game(){
-  var players = [];
-  var rounds = 0;
-  var totalRounds = 15;
-  this.initializeGame = function(){
-    var playerCount = prompt('How many players? Pick a number from 1 to 4.');
-    playerCount = parseInt(playerCount);
-    DrawPile.fillDeck(); //fill DrawPil with all 52 cards
-    this.createPlayers(playerCount); //create all player objects
-    this.playGame(); //begin the game
-  }
-  this.createPlayers = function(number){
-    for(var playerNumber = 0 ; playerNumber < number ; playerNumber++){
-      player = new Player(); //create new player
-      player.receiveCard(3); //have player fill hand with 3 cards
-      this.players.push(player); //push player to array
-    }
-  }
-  this.playGame = function(){
-    while(this.rounds < this.totalRounds){
-      for(var player = 0 ; player < players.length ; players++){
-        this.players[player].beginTurn(); //call function of current player to begin turn
-      }
-    }
-  }
+function Game() {
 
-  function Discard(name){
-    this.discardPile = [];
-    this.receiveCards = function(receivedCards){
-        //receive cards from player add them to discard
-        discardPile.concat(receivedCards);
+    var players = [];
+    var rounds = 0;
+    var totalRounds = 15;
+
+    this.initializeGame = function () {
+        var playerCount = prompt('How many players? Pick a number from 1 to 4.');
+        playerCount = parseInt(playerCount);
+        drawPile.fillDeck(); //fill DrawPil with all 52 cards
+        this.createPlayers(playerCount); //create all player objects
+        this.playGame(); //begin the game
     };
-    this.sendToDraw = function(cardsToSend){
-        var deck = this.discardPile.slice();
+
+    this.createPlayers = function (number) {
+        for (var playerNumber = 0; playerNumber < number; playerNumber++) {
+            player = new Player(); //create new player
+            player.receiveCard(3); //have player fill hand with 3 cards
+            this.players.push(player); //push player to array
+        }
+    };
+
+    this.playGame = function () {
+        while (this.rounds < this.totalRounds) {
+            for (var player = 0; player < players.length; players++) {
+                this.players[player].beginTurn(); //call function of current player to begin turn
+            }
+        }
+    };
+
+    function Discard(name) {
         this.discardPile = [];
-        return deck;
+        this.receiveCards = function (receivedCards) {
+            //receive cards from player add them to discard
+            discardPile.concat(receivedCards);
+        };
+        this.sendToDraw = function (cardsToSend) {
+            var deck = this.discardPile.slice();
+            this.discardPile = [];
+            return deck;
+        };
     };
 }
