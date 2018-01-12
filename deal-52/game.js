8a059d7e1211c0370bc23167b8096c80da0decdd
Aaron Guillen <aaronguillen21@gmail.com>
2018-01-12 11:37:01 -0800

    started Player object

--- deal-52/game.js
+++ deal-52/game.js
@@ -1,1 +1,14 @@
+function Player(name){
+    this.hand = [];
+    this.playerTurn = function(){
+      console.log(this.hand);
+      var cardsToRemove = prompt("Choose your cards to remove");
+    };
+    this.recieveCards = function (cards){
+        this.hand = this.hand.concat(cards);
+    };
+    this.sendCards = function(target){
+
+    };
+}
 
