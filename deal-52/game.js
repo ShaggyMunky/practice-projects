2a3eeb0a24ee9127fb2d3a0d45c0fc1003ad2ccd
Andrea <andrea.wayte@gmail.com>
2018-01-12 12:32:27 -0800

    combine all 4 objects

--- deal-52/game.js
+++ deal-52/game.js
@@ -6,20 +6,24 @@
  */
 
 function Game(){
-    var players = [];
-    var rounds = 0;
-    var totalRounds = 15;
+    this.players = [];
+    this.rounds = 0;
+    this.totalRounds = 15;
+    var drawPile;
+    var discardPile;
     this.initializeGame = function(){
         var playerCount = prompt('How many players? Pick a number from 1 to 4.');
         playerCount = parseInt(playerCount);
-        DrawPile.fillDeck(); //fill DrawPil with all 52 cards
+        drawPile = new DrawPile();
+        discardPile = new Discard();
+        drawPile.createDeck(); //fill DrawPil with all 52 cards
         this.createPlayers(playerCount); //create all player objects
         this.playGame(); //begin the game
     };
     this.createPlayers = function(number){
         for(var playerNumber = 0 ; playerNumber < number ; playerNumber++){
             player = new Player(); //create new player
-            player.receiveCard(3); //have player fill hand with 3 cards
+            player.drawCard(3); //have player fill hand with 3 cards
             this.players.push(player); //push player to array
         }
     };
@@ -30,6 +34,8 @@ function Game(){
             }
         }
     };
+    this.initializeGame();
+}
 /***************************************************************************************************
  *Player
  */
@@ -99,8 +105,6 @@ function DrawPile() {
 
 
 }
-
-var drawPile = new DrawPile();
 
 /***************************************************************************************************
  * Discard
